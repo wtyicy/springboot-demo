69051e30e494913644d0e6555e4169464afe5be2
568530214@qq.com <568530214@qq.com>
2019-12-13 15:14:33 +0800

    重新搭建

--- springboot-jpa/src/main/resources/sql/springboot_jpa.sql
+++ springboot-jpa/src/main/resources/sql/springboot_jpa.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v12.09 (64 bit)
 MySQL - 5.7.28-log : Database - springboot_jpa
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
